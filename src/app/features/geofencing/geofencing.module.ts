e7408e35b01759d1a46f3cea597d69b5dc7738c2
rupeshkjha <rupesh.jha.in@gmail.com>
2020-08-24 21:27:04 +0530

    Maps Added

--- src/app/features/geofencing/geofencing.module.ts
+++ src/app/features/geofencing/geofencing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AddfencingComponent } from "./addfencing/addfencing.component";
 import { GeofencingRoutingModule } from "./geofencing-routing.module";
+import { SharedModule } from "../../shared/shared.module";
 
 @NgModule({
   declarations: [AddfencingComponent],
-  imports: [CommonModule, GeofencingRoutingModule],
+  imports: [CommonModule, GeofencingRoutingModule, SharedModule],
 })
 export class GeofencingModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CellActionComponent } from './table/cell-action/cell-action.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CellActionComponent } from "./table/cell-action/cell-action.component";
+import { AgmCoreModule } from "@agm/core";
+import { GoogleMapsComponent } from "./google-maps/google-maps.component";
 
 @NgModule({
-  declarations: [CellActionComponent],
+  declarations: [CellActionComponent, GoogleMapsComponent],
   imports: [
     CommonModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyASyYRBZmULmrmw_P9kgr7_266OhFNinPA",
+      // To use the Google Maps JavaScript API, you must register your app project on the Google API Console and get a Google API key which you can add to your app
+    }),
   ],
-  exports: [CellActionComponent]
+  exports: [CellActionComponent, GoogleMapsComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
