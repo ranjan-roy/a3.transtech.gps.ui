ba386c1434699c85c3d166144aeb9cf758a9e1a2
Debasish Das <debasish.das.it@a3transtech.com>
2020-08-28 08:45:38 +0530

    prod map key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: "http://api.gps.a3transtech.com",
   storageType: "session",
-  mapKey: "",
+  mapKey: "AIzaSyD6MO9FB8MJMg59k_PxpMxXkwmIzLO4xzI",
 };
