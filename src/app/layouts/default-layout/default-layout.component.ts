3922f4ed5164497e1f722486a501722766ebe58a
rupeshkjha <rkjha.it.in@gmail.com>
2021-03-16 18:56:19 +0530

    side bar

--- src/app/layouts/default-layout/default-layout.component.ts
+++ src/app/layouts/default-layout/default-layout.component.ts
@@ -4,7 +4,11 @@ import { StorageService } from "../../core/service/storage.service";
 import { UserService } from "../../services/user.service";
 import { AuthService } from "../../core/service/auth.service";
 import { environment } from "../../../environments/environment";
-import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  LogLevel,
+} from "@microsoft/signalr";
 @Component({
   selector: "app-dashboard",
   templateUrl: "./default-layout.component.html",
@@ -15,32 +19,40 @@ export class DefaultLayoutComponent implements OnInit {
   private currentUser;
   userAccessLevel: number;
   private _hubConnection: HubConnection;
-  constructor(private storage: StorageService, private userService: UserService, public auth: AuthService) {}
+  constructor(
+    private storage: StorageService,
+    private userService: UserService,
+    public auth: AuthService
+  ) {}
 
   toggleMinimize(e) {
     this.sidebarMinimized = e;
   }
 
   ngOnInit() {
-    this.userService.getCurrentUser().subscribe(x=> {
+    this.userService.getCurrentUser().subscribe((x) => {
       this.currentUser = x;
       this._hubConnection = new HubConnectionBuilder()
-      .withUrl(`${environment.apiUrl}/notifierhub`)
-      .build();
+        .withUrl(`${environment.apiUrl}/notifierhub`)
+        .build();
 
-    this._hubConnection.on('broadcastNotification', (message) => {
-      console.log(message);
-    });
-    
-    this._hubConnection.start()
-      .then(() => {
-        this._hubConnection.invoke("OnConnected", x.userId); //1 = UserId
-        console.log('SignalR connection started');
-      })
-      .catch((err) => console.log('error while establishing signalr connection: ' + err));
+      this._hubConnection.on("broadcastNotification", (message) => {
+        console.log("broadcastNotification", message);
+      });
+
+      this._hubConnection
+        .start()
+        .then(() => {
+          this._hubConnection.invoke("OnConnected", x.userId); //1 = UserId
+          console.log("SignalR connection started");
+        })
+        .catch((err) =>
+          console.log("error while establishing signalr connection: " + err)
+        );
     });
     this.userAccessLevel = parseInt(this.storage.getItem("accessLevel"));
-    this.navItems = navItems.filter((nav) => this.auth.isAllowed(nav.name) || nav.title);
-    
+    this.navItems = navItems.filter(
+      (nav) => this.auth.isAllowed(nav.name) || nav.title
+    );
   }
 }
