e5daeef23aad6fc4698fc53922c171b4131ebe88
Debasish Das <debasish.das.it@a3transtech.com>
2021-03-16 15:32:42 +0530

    SignelR fix

--- src/app/layouts/default-layout/default-layout.component.ts
+++ src/app/layouts/default-layout/default-layout.component.ts
@@ -16,36 +16,36 @@ export class DefaultLayoutComponent implements OnInit {
   private currentUser;
   userAccessLevel: number;
   private _hubConnection: HubConnection;
-  constructor(private storage: StorageService, private userService: UserService, public auth: AuthService) {}
+  constructor(private storage: StorageService, private userService: UserService, public auth: AuthService) { }
 
   toggleMinimize(e) {
     this.sidebarMinimized = e;
   }
 
   ngOnInit() {
-    this.userService.getCurrentUser().subscribe(x=> {
+    this.userService.getCurrentUser().subscribe(x => {
       this.currentUser = x;
       this._hubConnection = new HubConnectionBuilder()
-      .withUrl(`${environment.apiUrl}/notifierhub`)
-      .withAutomaticReconnect() 
-      .configureLogging(signalR.LogLevel.Debug)
-      .build();
+        .withUrl(`${environment.apiUrl}/notifierhub`, { accessTokenFactory: () => this.storage.getToken() })
+        .withAutomaticReconnect()
+        .configureLogging(signalR.LogLevel.Debug)
+        .build();
       this._hubConnection.keepAliveIntervalInMilliseconds = 1000 * 60 * 3; // Three minutes
       this._hubConnection.serverTimeoutInMilliseconds = 1000 * 60 * 6; // Six minutes
 
-    this._hubConnection.on('broadcastNotification', (message) => {
-      console.log(message);
-    });
-    
-    this._hubConnection.start()
-      .then(() => {
-        this._hubConnection.invoke("OnConnected", x.userId); //1 = UserId
-        console.log('SignalR connection started');
-      })
-      .catch((err) => console.log('error while establishing signalr connection: ' + err));
+      this._hubConnection.on('broadcastNotification', (message) => {
+        console.log(message);
+      });
+
+      this._hubConnection.start()
+        .then(() => {
+          //this._hubConnection.invoke("OnConnected", x.userId); //1 = UserId
+          console.info('SignalR connection started');
+        })
+        .catch((err) => console.log('error while establishing signalr connection: ' + err));
     });
     this.userAccessLevel = parseInt(this.storage.getItem("accessLevel"));
     this.navItems = navItems.filter((nav) => this.auth.isAllowed(nav.name) || nav.title);
-    
+
   }
 }
